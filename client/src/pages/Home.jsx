4909f2e3ceae029e1b636060107c864eff87cd56
Byteers <husain.m.offc@gmail.com>
2024-02-07 23:42:14 +0530

    Home Page  Designing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,97 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+/* eslint-disable no-unused-vars */
 
-export default function Home() {
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-page-laptop">
+      <div className="div">
+        <header className="header">
+          <div className="banner">
+            <img
+              className="abstract-design"
+              alt="Abstract design"
+              src="https://cdn.animaapp.com/projects/65c3bde345e5c82d5efe0c0d/releases/65c3be5940105c1dbd69f547/img/abstract-design.svg"
+            />
+            <p className="text">Discover Your Dream Property with Estatein</p>
+            <button className="text-button">Learn More</button>
+            <img
+              className="button"
+              alt="Button"
+              src="https://cdn.animaapp.com/projects/65c3bde345e5c82d5efe0c0d/releases/65c3be5940105c1dbd69f547/img/button.svg"
+            />
+          </div>
+          <div className="navigation-bar">
+            <img
+              className="logo"
+              alt="Logo"
+              src="https://cdn.animaapp.com/projects/65c3bde345e5c82d5efe0c0d/releases/65c3be5940105c1dbd69f547/img/logo.svg"
+            />
+            <button className="text-wrapper"></button>
+            <div className="text-2">Contact Us</div>
+            <div className="buttons-container">
+              <button className="div-wrapper"></button>
+              <div className="text-2">Home</div>
+              <div className="text-button-2">About Us</div>
+              <div className="text-button-2">Properties</div>
+            </div>
+            <div className="text-button-2">Services</div>
+          </div>
+        </header>
+        <div className="container">
+          <di className="sub-container">
+            <div className="container-2">
+              <div className="text-container">
+                <p className="heading">
+                  Discovering Your Dream Property With Estatein
+                </p>
+                <p className="text-wrapper-2">
+                  Your journey to finding the perfect begins here. Explore our
+                  listings to find the home that matches your Dream
+                </p>
+                <div className="sub-container-2">
+                  <div className="shape">
+                    <img
+                      src="https://cdn.animaapp.com/projects/65c3bde345e5c82d5efe0c0d/releases/65c3be5940105c1dbd69f547/img/icon.svg"
+                      alt="Icon"
+                      className="icon"
+                    />
+                    <div className="text-container-2">
+                      <div className="text-3">✨</div>
+                      <div className="text-4">D</div>
+                      <div className="text-5">i</div>
+                      <div className="overlap">
+                        <div className="text-6">s</div>
+                        <div className="overlap-group">
+                          <div className="text-7">c</div>
+                          <div className="overlap-2">
+                            <div className="text-8">o</div>
+                            <div className="text-9">v</div>
+                            <div className="text-10">e</div>
+                            <div className="text-11">r</div>
+                            <div className="text-12">{""}</div>
+                            <div className="text-13">Y</div>
+                            <div className="text-14">o</div>
+                            <div className="text-15">u</div>
+                          </div>
+                          <div className="text-16">r</div>
+                        </div>
+                        <div className="text-17">{""}</div>
+                      </div>
+                      <div className="text-18">D</div>
+                      <div className="text-19">r</div>
+                      <div className="text-20">s</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </di>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
